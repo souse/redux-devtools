30542b4fc737d1852ed7bb0d6b591bdb870efd80
drank <chenfeng_lian@126.com>
2017-03-08 16:06:45 +0800

    批量创建actionCreator

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,3 +9,20 @@ export function getCookie(name) {
 	  var parts = value.split('; ' + name + '=');
 	  if (parts.length == 2) return parts.pop().split(';').shift();
 }
+
+/**
+ * 返回一个actionCreator
+ * @param  {String}    type     action的type
+ * @param  {Array} argNames 对应的字段 一般为payload
+ * @return {Function}             返回一个创建action的函数
+ */
+export function makeActionCreator(type, ...argNames) {
+  	return function(...args) {
+		let action = { type };
+
+		argNames.forEach((arg, index) => {
+			action[argNames[index]] = args[index];
+		})
+		return action;
+  	}
+}
